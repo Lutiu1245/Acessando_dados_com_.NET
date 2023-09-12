9a3e88de288b3a61789fea5905d18402433ddd3a
Luan Henrique <99328979+Lutiu1245@users.noreply.github.com>
2023-09-12 17:57:23 -0300

    Insert

--- DataAcess/DataAcess/Program.cs
+++ DataAcess/DataAcess/Program.cs
@@ -11,12 +11,36 @@ public class Program
     {
         const string connectionString 
             = "Server=localhost,1433;Database=balta; User ID=sa;Password=1q2w3e4r@#$";
+        var category = new Category();
+        category.Id = Guid.NewGuid();
+        category.Title = "Expert AWS";
+        category.Url = "Amazon";
+        category.Order = 8;
+        category.Description = "Aprenda AWS";
+        category.Summary = "AWS Cloud";
+        category.Featured = false;
+
+        var insertSql = @"INSERT INTO
+                        [Category]
+                            VALUES
+                        (@Id, @Title, @Url, @Summary, @Order, @Description, @Featured)";
         using (var connection = new SqlConnection(connectionString))
         {
-            var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
-            foreach (var category in categories)
+            var rows = connection.Execute(insertSql, new
             {
-                Console.WriteLine(category.Title);
+                category.Id,
+                category.Title,
+                category.Url,
+                category.Summary,
+                category.Order,
+                category.Description,
+                category.Featured,
+            });
+            Console.WriteLine(rows);
+            var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+            foreach (var item in categories)
+            {
+                Console.WriteLine(item.Title);
             }
         }
         Console.WriteLine("Hello");
