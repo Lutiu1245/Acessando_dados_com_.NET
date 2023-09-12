fd09bc21d7892171ce95da6885e0552563156a6e
Luan Henrique <99328979+Lutiu1245@users.noreply.github.com>
2023-09-12 18:18:15 -0300

    Separando CRUD por funções

--- DataAcess/DataAcess/Program.cs
+++ DataAcess/DataAcess/Program.cs
@@ -11,6 +11,25 @@ public class Program
     {
         const string connectionString 
             = "Server=localhost,1433;Database=balta; User ID=sa;Password=1q2w3e4r@#$";
+       
+        using (var connection = new SqlConnection(connectionString))
+        {
+            ReadCategories(connection);
+            //CreateCategorieList(connection);
+            UpdateCategorie(connection);
+        }
+        //Console.WriteLine("Hello");
+    }
+    static void ReadCategories(SqlConnection connection)
+    {
+        var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+        foreach (var item in categories)
+        {
+            Console.WriteLine(item.Title);
+        }
+    }
+    static void CreateCategorie(SqlConnection connection)
+    {
         var category = new Category();
         category.Id = Guid.NewGuid();
         category.Title = "Expert AWS";
@@ -24,25 +43,27 @@ public class Program
                         [Category]
                             VALUES
                         (@Id, @Title, @Url, @Summary, @Order, @Description, @Featured)";
-        using (var connection = new SqlConnection(connectionString))
+
+        var rows = connection.Execute(insertSql, new
         {
-            var rows = connection.Execute(insertSql, new
-            {
-                category.Id,
-                category.Title,
-                category.Url,
-                category.Summary,
-                category.Order,
-                category.Description,
-                category.Featured,
-            });
-            Console.WriteLine(rows);
-            var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
-            foreach (var item in categories)
-            {
-                Console.WriteLine(item.Title);
-            }
-        }
-        Console.WriteLine("Hello");
+            category.Id,
+            category.Title,
+            category.Url,
+            category.Summary,
+            category.Order,
+            category.Description,
+            category.Featured,
+        });
+        Console.WriteLine(rows);
+    }
+    static void UpdateCategorie(SqlConnection connection)
+    {
+        var UpdateQuery = @"UPDATE [Category] SET [Title]= @title WHERE [Id]=@id";
+        var row = connection.Execute(UpdateQuery, new
+        {
+            id = new Guid("af3407aa-11ae-4621-a2ef-2028b85507c4"),
+            title = "FrontEnd Master"
+        }) ;
+        Console.WriteLine("update"+ row);
     }
 }
